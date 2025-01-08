03fa004aa4b4f1d3a49abfc8413dc57293d5fcb5
Uali4494 <uali4494@gmail.com>
2025-01-08 14:47:32 +0500

    updated Profile

--- OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/education/education.component.ts
+++ OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/education/education.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../../Shared/Services/Global/global.service';
+import { CoursesSearchService } from '../../../Services/CourseSearch/CourseSearch.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ReportsService } from '../../../Services/Reports/reports.service';
+import { PagerService } from '../../../Shared/Services/Global/Pager';
+import { HighlightPipe } from '../../../Shared/Pipe/highlight.pipe';
+import { FilterPipe } from '../../../Shared/Pipe/filter';
+import { ProfileService } from './../../../Services/Profile/profile.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { InterconnectedService } from '../../../Shared/Services/Global/interconnected.service';
+declare const $: any;
 
 @Component({
   selector: 'app-education',
@@ -6,10 +19,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  educationForm: FormGroup;
+  constructor(
+    private _CoursesSearchService: CoursesSearchService,
+    private toastr: ToastrService,
+    private ngxService: NgxUiLoaderService,
+    private _ReportsService: ReportsService,
+    private formBuilder: FormBuilder,
+    private ProfileService: ProfileService,
+    private pagerService: PagerService,
+    private msgForDashboard: InterconnectedService
 
-  constructor() { }
+  ) {
+    this.educationForm = this.formBuilder.group({
+      FacultyMemberID: GlobalService.FacultyMember_ID,
+      eduInstitute: ['', Validators.required],
+      degree: ['', Validators.required],
+      field: ['', Validators.required],
+      year: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]]
+    });
+
+
+  }
 
   ngOnInit(): void {
   }
 
+  addEducation() {
+    if (this.educationForm.valid) {
+      const educationData = this.educationForm.value;
+      console.log('Education Data:', educationData);
+      // Call your service to save the data
+      this.ngxService.start();
+      this.ProfileService.AddFacultyEducation([educationData]).
+        subscribe(
+          data => {
+            this.ngxService.stop();
+            /* if (data) {*/
+            this.toastr.success("Education successfully", "Success");
+            $("#addFacultyEducation").modal("hide");
+
+            // this.msgForDashboard.UpdateCourseDetailsCounts(GlobalService.TempFacultyMember_ID.toString());
+            //}
+            //else {
+            //  console.log(data);
+            //  this.toastr.error("Error occured while processing your request.", "Error");
+            //}
+          },
+          error => {
+            this.ngxService.stop();
+            this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
+          });
+
+
+      this.educationForm.reset(); // Reset the form after submission
+    }
+  }
 }
--- OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/experience/experience.component.ts
+++ OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/experience/experience.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../../Shared/Services/Global/global.service';
+import { CoursesSearchService } from '../../../Services/CourseSearch/CourseSearch.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ReportsService } from '../../../Services/Reports/reports.service';
+import { PagerService } from '../../../Shared/Services/Global/Pager';
+import { HighlightPipe } from '../../../Shared/Pipe/highlight.pipe';
+import { FilterPipe } from '../../../Shared/Pipe/filter';
+import { ProfileService } from './../../../Services/Profile/profile.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { InterconnectedService } from '../../../Shared/Services/Global/interconnected.service';
+declare const $: any;
 
 @Component({
   selector: 'app-experience',
@@ -7,9 +20,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  experienceForm: FormGroup;
+
+  constructor(
+    private _CoursesSearchService: CoursesSearchService,
+    private toastr: ToastrService,
+    private ngxService: NgxUiLoaderService,
+    private _ReportsService: ReportsService,
+    private formBuilder: FormBuilder,
+    private ProfileService: ProfileService,
+    private pagerService: PagerService,
+    private msgForDashboard: InterconnectedService
+  ) {
+    this.experienceForm = this.formBuilder.group({
+      FacultyMemberID: GlobalService.FacultyMember_ID,
+      Position: ['', Validators.required],
+      Company: ['', Validators.required],
+      StartDate: ['', Validators.required],
+      EndDate: ['']
+    });
+
+  }
 
   ngOnInit(): void {
   }
+  addExperience() {
+    if (this.experienceForm.valid) {
+      const experienceData = this.experienceForm.value;
+      console.log('Education Data:', experienceData);
+      // Call your service to save the data
+      this.ngxService.start();
+      this.ProfileService.AddFacultyExperience([experienceData]).
+        subscribe(
+          data => {
+            this.ngxService.stop();
+            /* if (data) {*/
+            this.toastr.success("Experience added successfully", "Success");
+            $("#addFacultyExpereince").modal("hide");
 
+            // this.msgForDashboard.UpdateCourseDetailsCounts(GlobalService.TempFacultyMember_ID.toString());
+            //}
+            //else {
+            //  console.log(data);
+            //  this.toastr.error("Error occured while processing your request.", "Error");
+            //}
+          },
+          error => {
+            this.ngxService.stop();
+            this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
+          });
+
+
+      this.experienceForm.reset(); // Reset the form after submission
+    }
+  }
 }
