2a0d7ffea440b7cc2c641c1391ac1003d1ce9fe4
Uali4494 <46102821+Uali4494@users.noreply.github.com>
2025-01-20 07:24:02 +0500

    updated cv module

--- OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/education/education.component.js
+++ OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/education/education.component.js
@@ -2,6 +2,7 @@ import { __decorate } from "tslib";
 import { Component } from '@angular/core';
 import { GlobalService } from '../../../Shared/Services/Global/global.service';
 import { Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 let EducationComponent = class EducationComponent {
     constructor(_CoursesSearchService, toastr, ngxService, _ReportsService, formBuilder, ProfileService, pagerService, msgForDashboard) {
         this._CoursesSearchService = _CoursesSearchService;
@@ -12,7 +13,7 @@ let EducationComponent = class EducationComponent {
         this.ProfileService = ProfileService;
         this.pagerService = pagerService;
         this.msgForDashboard = msgForDashboard;
-        this.educationData = [];
+        this.educations = [];
         this.educationForm = this.formBuilder.group({
             FacultyMemberID: GlobalService.FacultyMember_ID,
             eduInstitute: ['', Validators.required],
@@ -28,8 +29,9 @@ let EducationComponent = class EducationComponent {
         const facultyMemberID = GlobalService.FacultyMember_ID;
         this.ProfileService.GetEducation(facultyMemberID).subscribe({
             next: (data) => {
-                this.educationData = data;
-                console.log("Education data" + this.educationData);
+                this.educations = data;
+                console.log(data);
+                console.log("Education data", this.educations);
             },
             error: (err) => {
                 console.error('Error fetching education data:', err);
@@ -40,19 +42,54 @@ let EducationComponent = class EducationComponent {
         if (this.educationForm.valid) {
             const educationData = this.educationForm.value;
             console.log('Education Data:', educationData);
-            // Call your service to save the data
             this.ngxService.start();
             this.ProfileService.AddFacultyEducation([educationData]).
                 subscribe(data => {
                 this.ngxService.stop();
                 this.toastr.success("Education successfully", "Success");
-                $("#addFacultyEducation").modal("hide");
+                $("#addEducationModal").modal("hide");
+                this.getEduction();
             }, error => {
                 this.ngxService.stop();
                 this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
             });
-            this.educationForm.reset(); // Reset the form after submission
+            this.educationForm.reset();
         }
+        else {
+            this.toastr.error("Please Enter All Fields.The value for year must be greater than 1900", "Error");
+        }
+    }
+    confirmDelete(eduID) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will not be able to recover this Education Data!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, keep it'
+        }).then((result) => {
+            if (result.value) {
+                this.ProfileService.DeleteEducation(eduID).subscribe(() => {
+                    Swal.fire('Deleted!', 'Your Education Data has been deleted.', 'success');
+                    this.getEduction();
+                }, error => {
+                    Swal.fire('Error!', 'Failed to delete Education Data.', 'error');
+                });
+            }
+        });
+        //  if (confirm('Are you sure you want to delete this Education Data?')) {
+        //    this.ProfileService.DeleteEducation(eduID).subscribe({
+        //      next: (response) => {
+        //        console.log('Delete Response:', response);
+        //        this.toastr.success("Education deleted successfully.", "Success");
+        //        this.getEduction();
+        //      },
+        //      error: (err) => {
+        //        console.error('Error deleting experience:', err);
+        //        this.toastr.error("Failed to delete education.", "Failed");
+        //      },
+        //    });
+        //  }
     }
 };
 EducationComponent = __decorate([
--- OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/profile/profile.component.js
+++ OBE_Portal/OBE-Portal-Client/src/app/Features/cv-module/profile/profile.component.js
@@ -2,7 +2,7 @@ import { __decorate } from "tslib";
 import { Component } from '@angular/core';
 import { GlobalService } from '../../../Shared/Services/Global/global.service';
 let ProfileComponent = class ProfileComponent {
-    constructor(_CoursesSearchService, toastr, ngxService, _ReportsService, formBuilder, ProfileService, pagerService, msgForDashboard) {
+    constructor(_CoursesSearchService, toastr, ngxService, _ReportsService, formBuilder, ProfileService, pagerService, msgForDashboard, cdr) {
         this._CoursesSearchService = _CoursesSearchService;
         this.toastr = toastr;
         this.ngxService = ngxService;
@@ -11,17 +11,40 @@ let ProfileComponent = class ProfileComponent {
         this.ProfileService = ProfileService;
         this.pagerService = pagerService;
         this.msgForDashboard = msgForDashboard;
+        this.cdr = cdr;
         this.faculty = {
             FacultyMemberID: 0,
             phone: '',
             FacultyType: '',
             FacultyRole: '',
         };
-        this.FacultyType = ['Permanent', 'Visiting', 'Adjunct'];
+        //facultydata: any = {
+        //  FacultyMemberID :0,
+        //  Name :"",
+        //  Email :"" ,
+        //  Phone:"",
+        //  FacultyType :"",
+        //  FacultyRole :"",
+        //};
+        this.facultydata = null;
+        this.FacultyType = ['Permanent/Participating', 'Visiting/Supporting'];
+        this.FacultyRole = ['SA', 'PA', 'SP', 'IP', 'Additional'];
     }
     ngOnInit() {
+        this.getFaculty();
         this.username = GlobalService.Name;
         this.faculty.FacultyMemberID = GlobalService.FacultyMember_ID;
+        this.faculty.phone = this.facultydata.phone;
+    }
+    openFacultyModal() {
+        // Set values for the modal
+        this.faculty = {
+            phone: this.facultydata[0].Phone,
+            FacultyType: this.facultydata[0].FacultyType,
+            FacultyRole: this.facultydata[0].FacultyRole,
+        };
+        // Open the modal (using jQuery for simplicity, you can replace with Angular modal handling)
+        $('#addFacultyModal').modal('show');
     }
     addFaculty() {
         console.log(this.faculty);
@@ -30,14 +53,24 @@ let ProfileComponent = class ProfileComponent {
         this.ProfileService.AddFacultyData([this.faculty]).
             subscribe(data => {
             this.ngxService.stop();
-            if (data) {
-                this.toastr.success("CLOs added successfully", "Success");
-                $("#addFacultyModal").modal("hide");
-                // this.msgForDashboard.UpdateCourseDetailsCounts(GlobalService.TempFacultyMember_ID.toString());
-            }
-            else {
-                this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
-            }
+            this.toastr.success("Profile Updated successfully", "Success");
+            $("#addFacultyModal").modal("hide");
+            this.getFaculty();
+        }, error => {
+            this.ngxService.stop();
+            this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
+        });
+    }
+    getFaculty() {
+        console.log(this.faculty);
+        this.ngxService.start();
+        this.ProfileService.GetFacultyDetails(GlobalService.FacultyMember_ID).
+            subscribe(data => {
+            this.ngxService.stop();
+            this.facultydata = data;
+            this.email = data.Email;
+            console.log(this.facultydata);
+            console.log(this.facultydata[0].Email);
         }, error => {
             this.ngxService.stop();
             this.toastr.error("Error occured while processing your request. Please contact to admin", "Error");
